bf77c34dc77b95606a94a66bd409c1395365b694
anamarialoga <anamarialogaa82@gmail.com>
2022-06-15 21:10:15 +0300

    fixes

--- backend/server.js
+++ backend/server.js
@@ -11,27 +11,29 @@ connectDB(); //connect to database
 const app = express();
 
 app.use(cors());
-app.use(express.json());//send raw json
-app.use(express.urlencoded({extended: false}));//send urlencoded
+app.use(express.json());//configure the app to send raw json
+app.use(express.urlencoded({extended: false}));
 
-//connect with the route file, which contains the strings to be added to the route;
+//connect with the route files
 app.use('/api/users', require('./routes/userRoutes') );
 app.use('/api/listings', require('./routes/listingRoutes'));
 app.use('/api/bookings', require('./routes/bookingRoutes'));
 app.use('/api/chat', require('./routes/chatRoutes'));
 
-//serve front-end
-if (process.env.NODE_ENV === 'production') {
-    // Set build folder as static
-    app.use(express.static(path.join(__dirname, '../frontend/build')))
-    app.get('*', (_, res) => {
-      res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
-    })
-  } else {
-    app.get('/', (req, res) => {
-      res.status(200).json({ message: 'Welcome to the Support Desk API' })
-    })
-}
-
 
 app.listen(PORT, ()=>console.log('SERVER STARTED ON PORT ', PORT));
+
+
+
+//serve front-end
+if (process.env.NODE_ENV === 'production') {
+  // Set build folder as static
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
+  app.get('*', (_, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+  })
+} else {
+  app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Welcome to the Support Desk API' })
+  })
+}
